4ec706d6f24694cb563e220acd9a4ab2a8f1ecce
ariceliom <contato@ariceliomoura.com>
2023-09-20 21:22:32 -0300

    Update jogoDaMemoria.js

--- 5 -  Projeto final Construindo um jogo da Memória/aula08-mostrar herois escontidos e timer/src/jogoDaMemoria.js
+++ 5 -  Projeto final Construindo um jogo da Memória/aula08-mostrar herois escontidos e timer/src/jogoDaMemoria.js
@@ -14,6 +14,7 @@ class JogoDaMemoria {
             { img: './arquivos/mulhermaravilha.png', nome: 'mulhermaravilha' },
         ]
 
+        
         this.heroisEscondidos = []
         this.heroisSelecionados = []
     }
